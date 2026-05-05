2b982c4f1e43009f037cd49d0d52f80c049f49d7
Mayursinh Zala <88233301+Mayursinh01@users.noreply.github.com>
2026-05-05 14:37:15 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,9 +2,11 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-from os import environ
+import os
+from dotenv import load_dotenv
 
-API_ID = int(environ.get("API_ID", "39823018"))
-API_HASH = environ.get("API_HASH", "507ed8ef0ad2779432caba373a66b413")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+load_dotenv()
 
+API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
